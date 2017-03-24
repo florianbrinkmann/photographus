7fa36c0052c85451b8d9e5834c4413bf38972908
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-03-24 14:56:31 +0100

    Fixed a few issues with the coding standards

--- index.php
+++ index.php
@@ -3,7 +3,10 @@
  * Main Template file
  *
  * @version 1.0.0
+ *
+ * @package Photographia
  */
+
 get_header(); ?>
 <main>Content</main>
 <?php get_footer(); ?>
--- tests/test-active-theme.php
+++ tests/test-active-theme.php
@@ -14,17 +14,17 @@ include_once( 'functions.php' );
  */
 class TestActiveTheme extends WP_UnitTestCase {
 
-    /**
-     * Return true if the currently active theme is Photographia
-     */
-    function test_active_theme() {
-        $this->assertTrue( 'Photographia' == wp_get_theme() );
-    }
+	/**
+	 * Return true if the currently active theme is Photographia
+	 */
+	function test_active_theme() {
+		$this->assertTrue( 'Photographia' === wp_get_theme() );
+	}
 
-    /**
-     * Return false if the currently active theme is the default theme Twenty Seventeen
-     */
-    function test_inactive_theme() {
-        $this->assertFalse( 'Twenty Seventeen' == wp_get_theme() );
-    }
+	/**
+	 * Return false if the currently active theme is the default theme Twenty Seventeen
+	 */
+	function test_inactive_theme() {
+		$this->assertFalse( 'Twenty Seventeen' === wp_get_theme() );
+	}
 }
