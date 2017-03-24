9dce610a171dbe72a10b9ce4a189d260db4c6cbf
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-03-24 12:01:52 +0100

    Added first small Unit Test

---
+++ tests/test-active-theme.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @see https://code.tutsplus.com/articles/the-beginners-guide-to-unit-testing-building-testable-themes--wp-26007
+ *
+ * @package Photographia
+ */
+
+include_once( 'functions.php' );
+
+/**
+ * Sample test case.
+ */
+class TestActiveTheme extends WP_UnitTestCase {
+
+    /**
+     * Return true if the currently active theme is Photographia
+     */
+    function test_active_theme() {
+        $this->assertTrue( 'Photographia' == wp_get_theme() );
+    }
+
+    /**
+     * Return false if the currently active theme is the default theme Twenty Seventeen
+     */
+    function test_inactive_theme() {
+        $this->assertFalse( 'Twenty Seventeen' == wp_get_theme() );
+    }
+}
