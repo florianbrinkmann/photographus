90a7d1298a29c9bb7db7ed47e526a9f773b68d1e
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-03-25 15:14:49 +0100

    Next try

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -8,7 +8,11 @@
 /**
  * Autoload the PHPunit
  */
-require_once 'PHPUnit/Autoload.php';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+require_once $_tests_dir . '/includes/testcase.php';
 
 /**
  * Sample test case.
