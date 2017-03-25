aa107dc82f82d7186be9dad52baec49f83e31ed9
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-03-25 12:00:28 +0100

    …

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,12 +5,6 @@
  * @package Photographia
  */
 
-if ( ! class_exists( '\PHPUnit\Framework\TestCase' ) &&
-     class_exists( '\PHPUnit_Framework_TestCase' )
-) {
-	class_alias( '\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase' );
-}
-
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
--- tests/test-active-theme.php
+++ tests/test-active-theme.php
@@ -12,6 +12,13 @@
  */
 include_once( 'functions.php' );
 
+// backward compatibility
+if ( ! class_exists( '\PHPUnit\Framework\TestCase' ) &&
+     class_exists( '\PHPUnit_Framework_TestCase' )
+) {
+	class_alias( '\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase' );
+}
+
 /**
  * Sample test case.
  */
