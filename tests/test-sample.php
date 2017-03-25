8fb9c7dadec96d0a91d4e64ad42404ac36e65cfe
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-03-25 15:22:30 +0100

    Try to install older PHPunit version

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -6,15 +6,6 @@
  */
 
 /**
- * Autoload the PHPunit
- */
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
-require_once $_tests_dir . '/includes/testcase.php';
-
-/**
  * Sample test case.
  */
 class SampleTest extends WP_UnitTestCase {
