6a91af4d9e7a7f654bdc1d47b6066f455996410b
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-03-30 17:30:53 +0200

    index.php first draft.

--- index.php
+++ index.php
@@ -8,5 +8,38 @@
  */
 
 get_header(); ?>
-<main>Content</main>
+<div class="content-wrapper clearfix">
+	<div class="main">
+		<main>
+			<?php
+			/**
+			 * Check if we have posts.
+			 */
+			if ( have_posts() ) {
+				/**
+				 * Loop the posts.
+				 */
+				while ( have_posts() ) {
+					/**
+					 * Setup the post data.
+					 */
+					the_post();
+
+					/**
+					 * Get the template part file. Default file is partials/post/content.php.
+					 * If available, use post format specific files (for example
+					 * partials/post/content-gallery.php) for Gallery format.
+					 */
+					get_template_part( 'partials/post/content', get_post_format() );
+				}
+			} else {
+				/**
+				 * Include partials/post/content-none.php if no posts were found.
+				 */
+				get_template_part( 'partials/post/content', 'none' );
+			}
+			?>
+		</main>
+	</div>
+</div>
 <?php get_footer(); ?>
