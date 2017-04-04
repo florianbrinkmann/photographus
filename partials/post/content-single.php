d8323234a603d54699cddc859486561b9268277d
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-04-04 11:58:22 +0200

    Added partials/post/content-single.php

---
+++ partials/post/content-single.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template part for displaying content of normal posts on single view.
+ *
+ * @version 1.0.0
+ *
+ * @package Photographia
+ */
+
+?>
+<article <?php post_class( 'clearfix' ); ?>>
+	<?php photographia_the_entry_header( 'h1', false ); ?>
+	<div class="entry-content">
+		<?php
+		/**
+		 * Displays the post content.
+		 */
+		photographia_the_content(); ?>
+	</div>
+	<footer class="entry-footer">
+		<?php
+		/**
+		 * Displays the footer meta: categories, tags, comment count and trackback count.
+		 */
+		photographia_the_entry_footer_meta(); ?>
+	</footer>
+</article>
