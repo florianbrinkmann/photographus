14770562c8c300c7ff345f76978503e2cde5eb12
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-04-26 16:22:53 +0200

    Added settings and controls for the other types

--- inc/customizer.php
+++ inc/customizer.php
@@ -20,7 +20,7 @@ function photographia_customize_register( $wp_customize ) {
 	] );
 
 	$wp_customize->add_setting( 'photographia_header_layout', [
-		'default'           => 0,
+		'default'           => false,
 		'sanitize_callback' => 'photographia_sanitize_checkbox',
 	] );
 
@@ -41,6 +41,52 @@ function photographia_customize_register( $wp_customize ) {
 	 */
 	$num_sections = apply_filters( 'photographia_front_page_sections', 4 );
 
+
+	/**
+	 * Filter number of posts which are displayed in the post panel dropdown.
+	 *
+	 * @param int $post_number Number of posts.
+	 */
+	$post_number = apply_filters( 'photographia_front_page_posts_number', 500 );
+
+	/**
+	 * Get the last posts for the dropdown menu for post panels.
+	 */
+	$post_panel_posts = new WP_Query( [
+		'post_type'      => 'post',
+		'posts_per_page' => $post_number,
+		'no_found_rows'  => true,
+	] );
+
+	/**
+	 * Build the choices array for the post panel.
+	 */
+	$post_panel_choices = [];
+	if ( $post_panel_posts->have_posts() ) {
+		while ( $post_panel_posts->have_posts() ) {
+			$post_panel_posts->the_post();
+
+			$post_panel_choices[ get_the_ID() ] = get_the_title();
+		}
+	}
+
+	/**
+	 * Build the choices array for the post grid panel category.
+	 *
+	 * @link https://blog.josemcastaneda.com/2015/05/13/customizer-dropdown-category-selection/
+	 */
+	$cats = [];
+	foreach ( get_categories() as $categories => $category ) {
+		$cats[ $category->term_id ] = $category->name;
+	}
+
+	/**
+	 * Add a value to the array so the user can choose a neutral value when he does not
+	 * want to show a post.
+	 */
+	$first_select_value = [ 0 => __( '— Select —', 'photographia' ) ];
+	$cats               = $first_select_value + $cats;
+
 	/**
 	 * Create a setting and control for each of the sections available in the theme.
 	 */
@@ -49,7 +95,7 @@ function photographia_customize_register( $wp_customize ) {
 		 * Create setting for saving the content type choice.
 		 */
 		$wp_customize->add_setting( "photographia_panel_{$i}_content_type", [
-			'default'           => 'page',
+			'default'           => 0,
 			'sanitize_callback' => 'photographia_sanitize_select',
 		] );
 
@@ -57,10 +103,12 @@ function photographia_customize_register( $wp_customize ) {
 		 * Create control for content choice.
 		 */
 		$wp_customize->add_control( "photographia_panel_{$i}_content_type", [
-			'label'   => "Panel $i",
+			/* translators: i = number of panel in customizer */
+			'label'   => __( "Panel $i", 'photographia' ),
 			'type'    => 'select',
 			'section' => 'photographia_options',
 			'choices' => [
+				0              => __( '— Select —', 'photographia' ),
 				'page'         => __( 'Page', 'photographia' ),
 				'post'         => __( 'Post', 'photographia' ),
 				'latest-posts' => __( 'Latest Posts', 'photographia' ),
@@ -80,16 +128,178 @@ function photographia_customize_register( $wp_customize ) {
 		 * Create control for page.
 		 */
 		$wp_customize->add_control( "photographia_panel_{$i}_page", [
-			'label'           => 'Select page',
+			'label'           => __( 'Select page', 'photographia' ),
 			'section'         => 'photographia_options',
 			'type'            => 'dropdown-pages',
 			'allow_addition'  => true,
 			'active_callback' => 'photographia_is_page_panel',
 			'input_attrs'     => [
-				'data-number' => $i,
+				'data-panel-number' => $i,
 			],
 		] );
-	}
+
+		/**
+		 * Check if we have posts to show, before creating the post controls and settings.
+		 */
+		if ( ! empty( $post_panel_choices ) ) {
+			/**
+			 * Add a value to the array so the user can choose a neutral value when he does not
+			 * want to show a post.
+			 */
+			$first_select_value = [ 0 => __( '— Select —', 'photographia' ) ];
+			$post_panel_choices = $first_select_value + $post_panel_choices;
+
+			/**
+			 * Create setting for post.
+			 */
+			$wp_customize->add_setting( "photographia_panel_{$i}_post", [
+				'default'           => false,
+				'sanitize_callback' => 'absint',
+			] );
+
+			/**
+			 * Create control for post.
+			 */
+			$wp_customize->add_control( "photographia_panel_{$i}_post", [
+				'label'           => __( 'Select post', 'photographia' ),
+				'section'         => 'photographia_options',
+				'type'            => 'select',
+				'choices'         => $post_panel_choices,
+				'active_callback' => 'photographia_is_post_panel',
+				'input_attrs'     => [
+					'data-panel-number' => $i,
+				],
+			] );
+
+			/**
+			 * Create setting for latest posts.
+			 */
+			$wp_customize->add_setting( "photographia_panel_{$i}_latest_posts", [
+				'default'           => 5,
+				'sanitize_callback' => 'absint',
+			] );
+
+			/**
+			 * Create control for latest posts.
+			 */
+			$wp_customize->add_control( "photographia_panel_{$i}_latest_posts", [
+				'label'           => __( 'Number of posts', 'photographia' ),
+				'section'         => 'photographia_options',
+				'type'            => 'text',
+				'active_callback' => 'photographia_is_latest_posts_panel',
+				'input_attrs'     => [
+					'data-panel-number' => $i,
+				],
+			] );
+
+			/**
+			 * Create setting to only show title and header meta of latest posts.
+			 */
+			$wp_customize->add_setting( "photographia_panel_{$i}_latest_posts_short_version", [
+				'default'           => false,
+				'sanitize_callback' => 'photographia_sanitize_checkbox',
+			] );
+
+			/**
+			 * Create control to only show title and header meta of latest posts.
+			 */
+			$wp_customize->add_control( "photographia_panel_{$i}_latest_posts_short_version", [
+				'label'           => __( 'Only show title and header meta of posts.', 'photographia' ),
+				'section'         => 'photographia_options',
+				'type'            => 'checkbox',
+				'active_callback' => 'photographia_is_latest_posts_panel',
+				'input_attrs'     => [
+					'data-panel-number' => $i,
+				],
+			] );
+
+			/**
+			 * Create setting for latest posts.
+			 */
+			$wp_customize->add_setting( "photographia_panel_{$i}_post_grid", [
+				'default'           => 20,
+				'sanitize_callback' => 'absint',
+			] );
+
+			/**
+			 * Create control for latest posts.
+			 */
+			$wp_customize->add_control( "photographia_panel_{$i}_post_grid", [
+				'label'           => __( 'Number of posts (skips posts without post thumbnail)', 'photographia' ),
+				'section'         => 'photographia_options',
+				'type'            => 'text',
+				'active_callback' => 'photographia_is_post_grid_panel',
+				'input_attrs'     => [
+					'data-panel-number' => $i,
+				],
+			] );
+
+			/**
+			 * Create setting to only show title and header meta of latest posts.
+			 */
+			$wp_customize->add_setting( "photographia_panel_{$i}_post_grid_hide_title", [
+				'default'           => false,
+				'sanitize_callback' => 'photographia_sanitize_checkbox',
+			] );
+
+			/**
+			 * Create control to only show title and header meta of latest posts.
+			 */
+			$wp_customize->add_control( "photographia_panel_{$i}_post_grid_hide_title", [
+				'label'           => __( 'Hide post titles.', 'photographia' ),
+				'section'         => 'photographia_options',
+				'type'            => 'checkbox',
+				'active_callback' => 'photographia_is_post_grid_panel',
+				'input_attrs'     => [
+					'data-panel-number' => $i,
+				],
+			] );
+
+			/**
+			 * Create setting to only show title and header meta of latest posts.
+			 */
+			$wp_customize->add_setting( "photographia_panel_{$i}_post_grid_only_gallery_and_image_posts", [
+				'default'           => false,
+				'sanitize_callback' => 'photographia_sanitize_checkbox',
+			] );
+
+			/**
+			 * Create control to only show title and header meta of latest posts.
+			 */
+			$wp_customize->add_control( "photographia_panel_{$i}_post_grid_only_gallery_and_image_posts", [
+				'label'           => __( 'Only display posts with post format »Gallery« or »Image«.', 'photographia' ),
+				'section'         => 'photographia_options',
+				'type'            => 'checkbox',
+				'active_callback' => 'photographia_is_post_grid_panel',
+				'input_attrs'     => [
+					'data-panel-number' => $i,
+				],
+			] );
+
+
+			/**
+			 * Create setting for post grid category.
+			 */
+			$wp_customize->add_setting( "photographia_panel_{$i}_post_grid_category", [
+				'default'           => false,
+				'sanitize_callback' => 'absint',
+			] );
+
+			/**
+			 * Create control for post grid category.
+			 */
+			$wp_customize->add_control( "photographia_panel_{$i}_post_grid_category", [
+				'label'           => __( 'Only show posts from one category:', 'photographia' ),
+				'section'         => 'photographia_options',
+				'type'            => 'select',
+				'choices'         => $cats,
+				'active_callback' => 'photographia_is_post_grid_panel',
+				'input_attrs'     => [
+					'data-panel-number' => $i,
+				],
+			] );
+		} // End if().
+	} // End for().
 
 	/**
 	 * Change transport to refresh
@@ -156,7 +366,7 @@ function photographia_sanitize_select( $input, $setting ) {
 }
 
 /**
- * Check if we need to display the dropdown-pages control for the panel.
+ * Check if we need to display the dropdown-pages control for a panel.
  *
  * @param WP_Customize_Control $control Control object.
  *
@@ -166,7 +376,7 @@ function photographia_is_page_panel( $control ) {
 	/**
 	 * Get the panel number from the input_attrs array, defined in the add_control() call.
 	 */
-	$panel_number = $control->input_attrs['data-number'];
+	$panel_number = $control->input_attrs['data-panel-number'];
 
 	/**
 	 * Get the value of the content type control.
@@ -177,6 +387,90 @@ function photographia_is_page_panel( $control ) {
 	 * Return true if the value is page.
 	 */
 	if ( 'page' === $content_type ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/**
+ * Check if we need to display the posts control for a panel.
+ *
+ * @param WP_Customize_Control $control Control object.
+ *
+ * @return bool
+ */
+function photographia_is_post_panel( $control ) {
+	/**
+	 * Get the panel number from the input_attrs array, defined in the add_control() call.
+	 */
+	$panel_number = $control->input_attrs['data-panel-number'];
+
+	/**
+	 * Get the value of the content type control.
+	 */
+	$content_type = $control->manager->get_setting( "photographia_panel_{$panel_number}_content_type" )->value();
+
+	/**
+	 * Return true if the value is page.
+	 */
+	if ( 'post' === $content_type ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/**
+ * Check if we need to display the latest posts control for a panel.
+ *
+ * @param WP_Customize_Control $control Control object.
+ *
+ * @return bool
+ */
+function photographia_is_latest_posts_panel( $control ) {
+	/**
+	 * Get the panel number from the input_attrs array, defined in the add_control() call.
+	 */
+	$panel_number = $control->input_attrs['data-panel-number'];
+
+	/**
+	 * Get the value of the content type control.
+	 */
+	$content_type = $control->manager->get_setting( "photographia_panel_{$panel_number}_content_type" )->value();
+
+	/**
+	 * Return true if the value is page.
+	 */
+	if ( 'latest-posts' === $content_type ) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/**
+ * Check if we need to display the latest posts control for a panel.
+ *
+ * @param WP_Customize_Control $control Control object.
+ *
+ * @return bool
+ */
+function photographia_is_post_grid_panel( $control ) {
+	/**
+	 * Get the panel number from the input_attrs array, defined in the add_control() call.
+	 */
+	$panel_number = $control->input_attrs['data-panel-number'];
+
+	/**
+	 * Get the value of the content type control.
+	 */
+	$content_type = $control->manager->get_setting( "photographia_panel_{$panel_number}_content_type" )->value();
+
+	/**
+	 * Return true if the value is page.
+	 */
+	if ( 'post-grid' === $content_type ) {
 		return true;
 	} else {
 		return false;
