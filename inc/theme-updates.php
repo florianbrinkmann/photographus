28e77bdd04ed17c9e93e7d778d64a659468344a1
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-05-19 13:25:58 +0200

    Additional doc improvements in theme-updates.php

--- inc/theme-updates.php
+++ inc/theme-updates.php
@@ -20,13 +20,22 @@ add_action( 'switch_theme', 'photographus_remove_upgrade_url', 10, 2 );
  * @param WP_Customize_Manager $wp_customize The Customizer object.
  */
 function photographus_update_customize_register( $wp_customize ) {
-	$wp_customize->add_setting( 'photographus_upgrade_url', [
-		'type'              => 'option',
-		'default'           => '',
-		'sanitize_callback' => 'photographus_esc_update_url',
-	] );
-
+	/**
+	 * Only add setting and control if we are not on a multisite.
+	 */
 	if ( ! is_multisite() ) {
+		/**
+		 * Add setting for URL.
+		 */
+		$wp_customize->add_setting( 'photographus_upgrade_url', [
+			'type'              => 'option',
+			'default'           => '',
+			'sanitize_callback' => 'photographus_esc_update_url',
+		] );
+
+		/**
+		 * Add control for update URL.
+		 */
 		$wp_customize->add_control( 'photographus_upgrade_url', [
 			'priority' => 1,
 			'type'     => 'url',
@@ -44,10 +53,24 @@ function photographus_update_customize_register( $wp_customize ) {
  * @return string
  */
 function photographus_esc_update_url( $url ) {
-	$url     = esc_url_raw( $url );
+	/**
+	 * Escape the URL.
+	 */
+	$url = esc_url_raw( $url );
+
+	/**
+	 * Possible Update URL patterns.
+	 */
 	$pattern = '/^https:\/\/florianbrinkmann\.com\/(en\/)?\?download_file=|^https:\/\/(en\.)?florianbrinkmann\.de\/\?download_file=/';
+
+	/**
+	 * Check if we have a match.
+	 */
 	preg_match( $pattern, $url, $matches );
 
+	/**
+	 * If match, return the URL. Otherwise an empty string.
+	 */
 	if ( ! empty ( $matches ) ) {
 		return $url;
 	} else {
@@ -67,24 +90,68 @@ function photographus_theme_update( $transient ) {
 		return $transient;
 	}
 
+	/**
+	 * Get data of upgrade json on florianbrinkmann.com.
+	 */
 	$request = photographus_fetch_data_of_latest_version();
+
+	/**
+	 * Check if request is not valid and return the $transient.
+	 * Otherwise get the data body.
+	 */
 	if ( is_wp_error( $request ) || wp_remote_retrieve_response_code( $request ) !== 200 ) {
 		return $transient;
 	} else {
 		$response = wp_remote_retrieve_body( $request );
 	}
 
-	$data     = json_decode( $response );
+	/**
+	 * Decode json.
+	 */
+	$data = json_decode( $response );
+
+	/**
+	 * Get the theme ID and unset it from the data object.
+	 */
 	$theme_id = $data->theme_id;
 	unset( $data->theme_id );
+
+	/**
+	 * Check if new version is available.
+	 */
 	if ( version_compare( $transient->checked['photographus'], $data->new_version, '<' ) ) {
-		$transient->response['photographus']        = (array) $data;
+		/**
+		 * Add response array for Photographus in $transient object.
+		 */
+		$transient->response['photographus'] = (array) $data;
+
+		/**
+		 * Set the changelog URL.
+		 */
 		$transient->response['photographus']['url'] = __( 'https://florianbrinkmann.com/en/wordpress-themes/photographus/changelog/', 'photographus' );
 
+		/**
+		 * Get the update URL theme option.
+		 */
 		$theme_package = get_option( 'photographus_upgrade_url' );
+
+		/**
+		 * Check if we have a URL.
+		 */
 		if ( ! empty ( $theme_package ) ) {
+			/**
+			 * Upgrade URL pattern (this time with theme ID, not without like in the customizer).
+			 */
 			$pattern = '/^https:\/\/florianbrinkmann\.com\/(en\/)?\?download_file=' . $theme_id . '|^https:\/\/(en\.)?florianbrinkmann\.de\/\?download_file=' . $theme_id . '/';
+
+			/**
+			 * Check for match.
+			 */
 			preg_match( $pattern, $theme_package, $matches );
+
+			/**
+			 * If match, add the package. Otherwise do not.
+			 */
 			if ( ! empty ( $matches ) ) {
 				$transient->response['photographus']['package'] = $theme_package;
 			} else {
@@ -111,8 +178,19 @@ function photographus_fetch_data_of_latest_version() {
  * if the new theme is not Photographus or a child theme of Photographus.
  */
 function photographus_remove_upgrade_url() {
+	/**
+	 * Get theme object.
+	 */
 	$theme_object = wp_get_theme();
-	$template     = $theme_object->template;
+
+	/**
+	 * Get template.
+	 */
+	$template = $theme_object->template;
+
+	/**
+	 * Check if template is »photographus«.
+	 */
 	if ( 'photographus' === $template ) {
 
 	} else {
