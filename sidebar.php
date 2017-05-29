091fb1c99e3ea32075e676f9f6853cd1e5c37364
Florian Brinkmann <kontakt@florianbrinkmann.de>
2017-05-29 13:58:21 +0200

    Check for panels front page in sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -7,7 +7,11 @@
  * @package Photographus
  */
 
-if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
+/**
+ * Check if we have widgets in the sidebar and are not on the front page
+ * with panels.
+ */
+if ( is_active_sidebar( 'sidebar-1' ) && false === photographus_is_front_page_with_panels() ) { ?>
 	<aside class="sidebar" role="complementary">
 		<h2 class="screen-reader-text">
 			<?php /* translators: screen reader text for the sidebar */
