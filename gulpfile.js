b9134ea99200f985a36ba7765487b09ac11fa1e7
Florian Brinkmann <kontakt@florianbrinkmann.com>
2018-10-24 13:51:04 +0200

    Fix never-ending production Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,45 +4,57 @@ const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 const flipper = require('gulp-css-flipper');
-const runSequence = require('run-sequence');
 
-gulp.task('sass', function () {
-	return gulp.src('assets/css/scss/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass({indentWidth: 1, outputStyle: 'expanded', indentType: 'tab'}).on('error', sass.logError))
-		.pipe(autoprefixer({browsers: ['last 3 versions'],}))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('assets/css'));
-});
+function sassTask() {
+	return (
+		gulp
+			.src('assets/css/scss/*.scss')
+			.pipe(sourcemaps.init())
+			.pipe(sass({indentWidth: 1, outputStyle: 'expanded', indentType: 'tab'}).on('error', sass.logError))
+			.pipe(autoprefixer({browsers: ['last 3 versions'],}))
+			.pipe(sourcemaps.write('.'))
+			.pipe(gulp.dest('assets/css'))
+	);
+}
 
-gulp.task('css-rtl', function () {
-	return gulp.src(['assets/css/*.css', '!assets/css/*-rtl.css'])
-		.pipe(flipper())
-		.pipe(rename(
-			{suffix: "-rtl"}
-		))
-		.pipe(gulp.dest('assets/css/'));
-});
+function rtlTask() {
+	return (
+		gulp
+			.src(['assets/css/*.css', '!assets/css/*-rtl.css'])
+			.pipe(flipper())
+			.pipe(rename(
+				{suffix: "-rtl"}
+			))
+			.pipe(gulp.dest('assets/css/'))
+	);
+}
 
-gulp.task('sass-production', function () {
-	return gulp.src('assets/css/scss/*.scss')
-		.pipe(sass({indentWidth: 1, outputStyle: 'expanded', indentType: 'tab'}).on('error', sass.logError))
-		.pipe(autoprefixer({browsers: ['last 3 versions'],}))
-		.pipe(gulp.dest('assets/css'));
-});
+function sassProduction() {
+	return (
+		gulp
+			.src('assets/css/scss/*.scss')
+			.pipe(sass({indentWidth: 1, outputStyle: 'expanded', indentType: 'tab'}).on('error', sass.logError))
+			.pipe(autoprefixer({browsers: ['last 3 versions'],}))
+			.pipe(gulp.dest('assets/css'))
+	);
+}
+
+function watchTask() {
+	return gulp.watch('assets/css/scss/**/*.scss', gulp.series(
+		sassTask,
+		rtlTask
+	))
+}
 
 gulp.task('default', gulp.series(
-		'sass',
-		'css-rtl'
+		sassTask,
+		rtlTask,
+		watchTask,
 	),
-	gulp.watch('assets/css/scss/**/*.scss', gulp.series(
-		'sass',
-		'css-rtl'
-	))
 );
 
 gulp.task('production', gulp.series(
-		'sass-production',
-		'css-rtl'
-	)
+		sassProduction,
+		rtlTask
+	),
 );
